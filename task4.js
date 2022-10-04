4d7fd9dcdd0a9a3add875c91dc9f61778a87de6d
Chiksinafees <nafeesahmed01.na@gmail.com>
2022-10-04 18:43:48 +0530

    task4 done

---
+++ task4.js
@@ -0,0 +1,10 @@
+
+ items=document.getElementsByClassName('list-group-item')
+items[2].style.backgroundColor='green'
+//items[1].textContent='green'
+//console.log(items)
+
+for(let i=0; i<items.length; i++)
+{
+    items[i].style.fontWeight='bold'
+}
