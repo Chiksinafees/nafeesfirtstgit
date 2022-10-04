efd874b211a5092faf8bf0d197855c2694fe8901
Chiksinafees <nafeesahmed01.na@gmail.com>
2022-10-04 19:15:26 +0530

    task5 done

---
+++ task5.js
@@ -0,0 +1,22 @@
+/* let li=document.getElementsByClassName('li')
+li[2].style.backgroundColor='green'
+//items[1].textContent='green'
+//console.log(items)
+
+for(let i=0; i<li.length; i++)
+{
+    li[i].style.fontWeight='bold'
+}
+*/
+
+let li=document.getElementsByTagName('li')
+li[2].style.backgroundColor='green'
+//items[1].textContent='green'
+//console.log(items)
+
+for(let i=0; i<li.length; i++)
+{
+    li[i].style.fontWeight='bold'
+}
+
+
